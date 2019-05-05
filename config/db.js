21e97abe6c76faa3e058ff2b0ed19679edc98a1c
superian12 <christian.ian.banzon@gmail.com>
2019-05-06 00:43:41 +0800

    update new database-v8

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'ls-8b7b190fe9a79debba58ea98927b8912677e3a6d.cgs1hrdaq2sg.ap-southeast-1.rds.amazonaws.com',
+    host     : 'ls-964a7eb92c94dbdb7b95dea9c3308bd8bb682711.cgs1hrdaq2sg.ap-southeast-1.rds.amazonaws.com',
     user     : 'dbmasteruser',
     // host     : 'localhost',
     // user     : 'root',
